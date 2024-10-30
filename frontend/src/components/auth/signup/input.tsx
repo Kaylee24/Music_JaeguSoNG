6675e27b5fd0be503f83deddd3633cfca14d9699
박수빈 <subin57419@gmail.com>
2024-10-31 00:01:32 +0900

    회원가입 페이지 생성
    Former-commit-id: 298502f8946526dfaba5a59393725d8c56c05cdf

--- frontend/src/components/auth/signup/input.tsx
+++ frontend/src/components/auth/signup/input.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-const Input: React.FC = () => {
-  return (
-    <div>
-      /* content */
-    </div>
-  );
-};
-
-export default Input;
+version https://git-lfs.github.com/spec/v1
+oid sha256:4a36f2978d00aadee73953feda793b3b727a104175b429a363952dd78bd0d583
+size 8930
