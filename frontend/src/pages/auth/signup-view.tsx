62d11fb7b7c732120b4ba262eb3e03243130b465
박수빈 <subin57419@gmail.com>
2024-10-30 15:40:40 +0900

    회원가입 타이틀 작성
    Former-commit-id: 84e227d863f0f11604c35a065fa75d3a7b05b918

--- frontend/src/pages/auth/signup-view.tsx
+++ frontend/src/pages/auth/signup-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3f0612fc3000102602ad6f6a65c7aa4e3e338421b6a1642978c0dc1e1dd5091
-size 74
+oid sha256:09b6f7e574e02cd9db24d5e0780e1333ece8cab7f088cc590a938cb949a2886c
+size 199
--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6858d66ff120d09dd0403e4ada93dce0c392a97c4a9fcb1f122c9c80057e7fb
-size 490
+oid sha256:c9767bbfdda254b99f968ea70bd5396593bc38da6437ebbb0a521448a7b93bfd
+size 424
