51794fe75d14c70db65b94d02a5dc7ec5b6da818
김송희 <kkbuh2@gmail.com>
2024-10-30 14:38:45 +0900

    Fe settings
    Former-commit-id: 601280b400f19a288f4d4612f652b88997a26da1

---
+++ frontend/src/components/ui/clipboard.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fad2c0fe0f4e6263a1c11c79c91b543057d4447fff76e143654f6d0d0a94e87c
+size 2645
---
+++ frontend/src/configs/paths.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:81c554307247f3b18ac634c493e781fdc5a84d22ade3d455633ce9a3e951eed5
+size 714
