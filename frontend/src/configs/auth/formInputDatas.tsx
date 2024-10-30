d0753e2afa6423d56c8b306c20913b5bc78715a1
박수빈 <subin57419@gmail.com>
2024-10-31 00:29:16 +0900

    로그인 페이지 생성
    Former-commit-id: d09c48fbe510dd9b652750a2aff8073a57c42449

--- frontend/src/configs/auth/formInputDatas.tsx
+++ frontend/src/configs/auth/formInputDatas.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aa5e5cdeaa0821974c5641329014071c3a63be4eccece5d9d4058f6599e871f
-size 2008
+oid sha256:4703c89f0f7984c47249f062e55e70150a857d1da5f59a8c99ea37dfc7d85363
+size 2478
--- frontend/src/pages/auth/signin-view.tsx
+++ frontend/src/pages/auth/signin-view.tsx
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8566b1f0e8d33baf96e2c2cbed6df2031c4716df4c6c75e5d37f0e6f7ce5c5fe
-size 196
+import { Box, Text } from '@chakra-ui/react';
+import Container from '@/components/auth/signin/container';
+
+export default function SignInView() {
+  return (
+    <Box color="white">
+      <Text fontFamily="MiceGothicBold">로그인</Text>
+      <Container />
+    </Box>
+  ) 
+}
