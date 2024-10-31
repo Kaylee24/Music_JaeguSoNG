12271bd7094284d205baac98337875f2f1fe9e2c
Songdoeon <ehdjs9583@naver.com>
2024-10-31 16:36:27 +0900

    [BE] SecurityConfig whiteList 추가
    Former-commit-id: 556aadf41b814580e1b4c1e22f76c0a91f19921c

--- src/main/java/com/e106/reco/global/config/SecurityConfig.java
+++ src/main/java/com/e106/reco/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1e3eca7f2fd3eb2d97d1227bb211e1af118107f2f783f038db827313e1eafbc
-size 3673
+oid sha256:0355ad54d2cd4277a2c6afa60c1b1d54652af9c96ae21570bc75fe7d3645a51d
+size 3992
