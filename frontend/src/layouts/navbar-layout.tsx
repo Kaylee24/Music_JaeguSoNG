b01470bd80fd64a045432eb89bd94a7ad6d0d6fd
김송희 <kkbuh2@gmail.com>
2024-10-31 17:06:57 +0900

    [FE] #2 navbar 기본 구조 생성
    Former-commit-id: 85d69b1830b43084c437072441e41978d1db860c

--- frontend/src/layouts/navbar-layout.tsx
+++ frontend/src/layouts/navbar-layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97c1339c74d4d02918b282ad556771686c1bd5d5ea1b472084f5bd90ead83888
-size 633
+oid sha256:db98bb4cbf20a173a2b49ecf298269e4386486cfe75477615ea69327a1324730
+size 662
