9ac5c0595d701239b7138d6cdfa92b82fab81737
김송희 <kkbuh2@gmail.com>
2024-10-31 15:33:41 +0900

    [FE] #2 divider, ws-list, ws-detail 기본 컴포넌트 및 구조 작업중
    Former-commit-id: c1a9925c1985153a9f700c7503a2afe479c9ddce

--- frontend/src/configs/paths.ts
+++ frontend/src/configs/paths.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81c554307247f3b18ac634c493e781fdc5a84d22ade3d455633ce9a3e951eed5
-size 714
+oid sha256:e2266c5f877cedbe7ae3fedd72b66796a26cd1d81a744c763b3cfaf0374e2f75
+size 887
