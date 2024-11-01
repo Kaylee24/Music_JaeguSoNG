fc637a627b7c9038e5983e7fe0cf4f5c764df1c7
송도언 <ehdjs9583@naver.com>
2024-11-01 17:50:44 +0900

    [BE] CORS.etc 제거
    Former-commit-id: caf77f6edfb756ffb7ff747f67fceca5e36d4f30

--- src/main/java/com/e106/reco/global/config/SecurityConfig.java
+++ src/main/java/com/e106/reco/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdc06a05d87f6cdeabbe9968cf3a913a3dd810e8fa6b348dce181c0e9b151bcd
-size 4080
+oid sha256:1068fa4b6e8e1684c61295e9c9ce60cd62ca95d21075ffff97b3933ff87f8481
+size 3991
--- src/main/java/com/e106/reco/global/config/WebConfig.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4c8736cba620b102c18859ab4158563b770be49c973ad1de67ebe85cf67e82bc
-size 760
