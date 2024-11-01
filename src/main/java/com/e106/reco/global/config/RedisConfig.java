6fff7f5c8e2def88393b66711036ab2071856636
송도언 <ehdjs9583@naver.com>
2024-11-01 12:18:02 +0900

    [BE] redis config 수정
    Former-commit-id: 5a258ca8f6984eaa51c7c0cd2d674acd6b37df8f

--- src/main/java/com/e106/reco/global/config/RedisConfig.java
+++ src/main/java/com/e106/reco/global/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40fd7e47c80bb29ebdeaed71332a54b0c892e0f4c54cfc2f3cd6009821d759df
-size 1020
+oid sha256:812913a0b762e2ae57526797982393fea49c3dcc5c02c7f7df91ccf10e586855
+size 1685
