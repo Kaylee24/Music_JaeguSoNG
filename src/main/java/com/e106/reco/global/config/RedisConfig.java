86118b8b8b4c198f3f1f5be8fd2b8d83036e2873
송도언 <ehdjs9583@naver.com>
2024-11-01 12:21:01 +0900

    [BE] redis config log
    Former-commit-id: 79953e66b596201925bca1b2364481ac1f3feee0

--- src/main/java/com/e106/reco/global/config/RedisConfig.java
+++ src/main/java/com/e106/reco/global/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:696e6ba9036ede15d44221483d34f62a9cfa3dd1531d0f27a05baf4d60a20e3f
-size 1728
+oid sha256:292121003284c407bb50fd7558afb89b2eea03875935d05a4ae8da5332127364
+size 1893
