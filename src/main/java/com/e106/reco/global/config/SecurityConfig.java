49e8c5566a868e09adfa89e80300e6c8c0b38e5b
송도언 <ehdjs9583@naver.com>
2024-11-01 11:55:31 +0900

    [BE] cors filter 수동 입력 제거
    Former-commit-id: f95924d77c840adaf8d1530e1c59722692b389b9

--- src/main/java/com/e106/reco/global/config/SecurityConfig.java
+++ src/main/java/com/e106/reco/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c6c5b4392543a49a953076d3421a4a2f3639fa472275aad9c97f5321387dd42
-size 4077
+oid sha256:ecea5126cd3eadb1e1749d1e19372a13ca895d53a549e3ea67faa3ef8e5def80
+size 3989
