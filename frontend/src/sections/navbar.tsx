9fd127279ae31ccd6552aa954f9fc76214d4e642
박수빈 <tns027@naver.com>
2024-11-01 14:33:49 +0900

    FE : 네비바 폰트 수정
    Former-commit-id: e799ef896f3fd17abb2f3de0e9feaedba7c053fd

--- frontend/src/sections/navbar.tsx
+++ frontend/src/sections/navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc2c9a9b38ba6b1aa3ae9e3e28ceb23de344f1ada835c05d35527963f28110dc
-size 2533
+oid sha256:a5053cf2d268fea8c19bb63de7dabbbe8c3ac110fa9750d67befe687876f1e0f
+size 2784
