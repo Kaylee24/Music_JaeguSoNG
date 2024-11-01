986b018d81ac2a965b702490f0e6706765116da0
박수빈 <tns027@naver.com>
2024-11-01 12:02:58 +0900

    FE 커뮤니티 수정
    Former-commit-id: 91f2d624a705ad02dc5c57c879e1796da75c42cd

--- frontend/src/components/community/header.tsx
+++ frontend/src/components/community/header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4a29f17cdfbe261f83048d06ada935a88b97989ec08f9cd78db325b63e83d2e
-size 1167
+oid sha256:6b6c63eb5ae0867bf503b81206e5549ee8d32a6e35de71c13029f7aa8a68a348
+size 1338
---
+++ frontend/src/components/community/update-input-container.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3281fa895e9df94d411478dd5fb2f30c31621ef4f78a9ae92aba03d825d7001
+size 2720
