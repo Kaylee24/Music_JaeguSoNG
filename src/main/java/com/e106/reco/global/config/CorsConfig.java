c6e3b573a52cf813c8c0e97a905243bd8a107d9b
송도언 <ehdjs9583@naver.com>
2024-11-01 17:17:44 +0900

    Update src/main/java/com/e106/reco/global/config/CorsConfig.java
    Former-commit-id: 45a81a8f9e60df19c9a9a08a4f1b5427b57bafa3

--- src/main/java/com/e106/reco/global/config/CorsConfig.java
+++ src/main/java/com/e106/reco/global/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9d42fea11bc472d12d7c5328837ea6989d6352a9bc19f605c109347c21a6f93
+oid sha256:0c0afe08989b9ca8edec45d252ea4907f3c8b9a33b352f2f93555a98e633e7ef
 size 1162
