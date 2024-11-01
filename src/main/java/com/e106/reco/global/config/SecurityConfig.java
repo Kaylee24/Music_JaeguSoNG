fc399d4426c5d6833047dba4d03521474efe174d
권혜경 <skygprud0707@gmail.com>
2024-11-01 17:24:29 +0900

    [BE] cors filter add
    Former-commit-id: cdb4dc33bd68ec3d7034b09f38d588e8d9c3f0f8

--- src/main/java/com/e106/reco/global/config/SecurityConfig.java
+++ src/main/java/com/e106/reco/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40460a6090149fe3f5a304a109d03f31922f4cd9d09581286f6ed9fe9eb8b5a5
-size 4073
+oid sha256:db0a99f3a37b10078b80b4dba26ab8f0be6a49dad8a29d36e6cbbb9658b3c615
+size 4167
