b27c5e8a6a0cf0c6729887627b36091fabb4195b
송도언 <ehdjs9583@naver.com>
2024-11-01 11:49:39 +0900

    [BE] corsFilter 수정
    Former-commit-id: ed80f4a1ea93ce0bf936d4cc9255bca940bb67b3

--- src/main/java/com/e106/reco/global/config/CorsConfig.java
+++ src/main/java/com/e106/reco/global/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da12ce9a72d21016f6ba6985cb4385de8981ba1d423f02478bc9400a4b0d34e0
-size 1227
+oid sha256:50495c88cdd3c9bd904dc4c9fe5f10df0b765ac0bd523122fa4223884beb2ac9
+size 1188
