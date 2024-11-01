bb1bc0be0090ca0c460ec3b2f0dfd94a6d290cd6
박수빈 <tns027@naver.com>
2024-11-01 15:00:09 +0900

    FE : 헤더 이상한 거 고침
    Former-commit-id: e98381d27b52114a42974573315ae0ad61929031

--- frontend/src/pages/community/community-create-view.tsx
+++ frontend/src/pages/community/community-create-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02fd9746c3807a0fb438c64fb526aa978f873aecf9120c726637f51e90585f7f
-size 244
+oid sha256:a49b2a25e4acd3cdcf47072dc5eda21d7949889e96fee4e71f7a2b1d31db582f
+size 406
