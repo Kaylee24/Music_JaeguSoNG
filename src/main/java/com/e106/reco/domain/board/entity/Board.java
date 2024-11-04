4f6d584e8217926039d3a9f9b211bb554f2a6e24
송도언 <ehdjs9583@naver.com>
2024-11-04 09:59:08 +0900

    Refactor : board 및 source id 생성 전략 identity로 변경
    Former-commit-id: e43261fc041b751a992fb8ae5fb139591b9ff620

--- src/main/java/com/e106/reco/domain/board/entity/Board.java
+++ src/main/java/com/e106/reco/domain/board/entity/Board.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b45e05f33d2a2db88b8b1cb51c62e1fc0ea6980fa6c8b04f40a969abba76b41
-size 1245
+oid sha256:f025b8f3f3bf88e24064e2b27ce2f74a088b9feb772ad6b442ba69084b762bfd
+size 1324
--- src/main/java/com/e106/reco/domain/board/entity/Source.java
+++ src/main/java/com/e106/reco/domain/board/entity/Source.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6717ddc84f87119accdaad1a857a1e077b59186b46f63b2d79b0868017d7a995
-size 726
+oid sha256:a457815b9cfb8dadca194f10edb88941d0f40188026115e96bb2ab5301c7f9df
+size 805
