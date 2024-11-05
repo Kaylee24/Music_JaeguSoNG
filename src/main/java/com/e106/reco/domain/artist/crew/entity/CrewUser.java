ef8535da5a2817537fa3e6588632eda5bb7ea4d7
KHG <skygprud0707@gmail.com>
2024-11-05 10:25:02 +0900

    feat : user 로그인 수정
    Former-commit-id: 1922c65a4cb31d97be15fbf9ed224f3ad737232e

--- src/main/java/com/e106/reco/domain/artist/crew/entity/CrewUser.java
+++ src/main/java/com/e106/reco/domain/artist/crew/entity/CrewUser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88f13c30341d4fa70f0690ac9bc0aecf32cd67e65e3b638d9480c048b76ade62
-size 2727
+oid sha256:dbeabef63dd728caacb818c056bf11aa2394b0f75a9e89e991fc3be66fdd2a2e
+size 2691
