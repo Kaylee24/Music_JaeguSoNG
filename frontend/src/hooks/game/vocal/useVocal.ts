7a64915a5d5110dc2622011bca0e714cedde4488
구현우 <kjklovekhw@naver.com>
2024-11-06 16:17:53 +0900

    [FE] #3 Game : 보컬 게임 구현
    Former-commit-id: 5bd14b9582d8c23e87ea48f7887bce120ac50e56

--- frontend/src/hooks/game/vocal/useVocal.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f32481ea2be89c7c240c233ad3fcda3e1206b74afb8ade28098ccd009039a76f
-size 2484
--- frontend/src/utils/game/keyboardsSound.ts
+++ frontend/src/utils/game/keyboardsSound.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dcebb6e6d101126d9e9849b7c7304d47a44b3af2ca81cf61c66b079f1f83f1f
+oid sha256:2f956bbda665470d01680f2aedee53686204692bb5442538ac07428db1b7c7a0
 size 1212
