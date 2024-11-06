5bfaad6b7e4a544ce2762f4a0bd3efcc9ce01ed0
권혜경 <skygprud0707@gmail.com>
2024-11-06 10:06:19 +0900

    Be s3file
    Former-commit-id: c73cbed7c0aba6c25cccc0d9173f4cf4a650c7d1

--- src/main/java/com/e106/reco/domain/artist/crew/entity/Crew.java
+++ src/main/java/com/e106/reco/domain/artist/crew/entity/Crew.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2febd6cc00d2c34fdfa44796872ad5aa47f924b9fc9ca1445db732ff2e13a5f9
-size 1957
+oid sha256:0a797d92b89d7eadb5ab8c9c903fc218fd20419e318ff6418d1366617c1a42aa
+size 1959
