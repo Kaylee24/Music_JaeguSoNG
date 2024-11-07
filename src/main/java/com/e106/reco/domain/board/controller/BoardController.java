b1b0917da2d84abce7087194cd65987ffeac788e
권혜경 <skygprud0707@gmail.com>
2024-11-07 14:45:08 +0900

    Update src/main/java/com/e106/reco/domain/board/controller/BoardController.java
    Former-commit-id: 724a2e3f65ece3b672e8fc933f938a0870d55557

--- src/main/java/com/e106/reco/domain/board/controller/BoardController.java
+++ src/main/java/com/e106/reco/domain/board/controller/BoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:754238e4ce4ece1d4dcf6ff7c6f55f48fe4d6d103655ebd77122bde913209c98
-size 2243
+oid sha256:d23b570dca84e38f4f2e614c5de727db3feae68a403e5cb2292ba91911c5a476
+size 2242
