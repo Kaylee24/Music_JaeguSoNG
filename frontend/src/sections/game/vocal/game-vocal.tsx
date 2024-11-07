4746902d359b780d3a5a6907f493cb0d41438457
구현우 <kjklovekhw@naver.com>
2024-11-07 15:20:48 +0900

    [FE] #3 fix : 보컬 게임 오류 수정
    Former-commit-id: 2b5fca8619137aaeff065974e9e4309bf1c0d160

--- frontend/src/sections/game/vocal/game-vocal.tsx
+++ frontend/src/sections/game/vocal/game-vocal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b4ba5b25573bbd367203f56f232948da5b9c8a101c32db817017b45a73f62f0
-size 3350
+oid sha256:86a838d4312c6acb642c9c21b2d42b2ba73c50914079e6a8ebdf773c49f095fc
+size 3434
