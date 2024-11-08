0773eee93c8d6100b1e956c3b457ee534db5524a
박수빈 <subin57419@gmail.com>
2024-11-08 17:10:11 +0900

    음원 피드 목록 api 연결 중
    Former-commit-id: a191a0ab499089dc54fccbf6545010285f1341cd

--- frontend/src/components/community/article-music-list.tsx
+++ frontend/src/components/community/article-music-list.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23f920140186c8782bbfa40f92eefc944461657bd300a14e5067ffbc61165267
-size 3805
+oid sha256:0d1e2e1682e0f493f262e6066bfa5bb93fb398a2fc1a3ab4999da5e22f8b3426
+size 2295
