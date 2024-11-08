ec7f90a05ffd663e61ac7413caf42e49921edd0a
구현우 <kjklovekhw@naver.com>
2024-11-08 11:53:28 +0900

    [FE] style : 네비바, 회원가입, 세팅페이지 css 수정
    Former-commit-id: c1ceb035551515893212972f60aa33c9f5ddefc5

--- frontend/src/sections/navbar.tsx
+++ frontend/src/sections/navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01132396f6c717338deea1404bbdb601fcb0d2feba176d2d1e6945953d6bca7a
-size 5343
+oid sha256:b06c4bafee94ec18c001d040b7e8aa44921313e8f34a21a3838e6104956d8bb2
+size 6325
