5cf32de1b2cc3968c30e9259d1d27e8116187846
박수빈 <tns027@naver.com>
2024-11-08 10:18:14 +0900

    FE : 마이페이지 크루 목록 api 연결
    Former-commit-id: 94d47f72fd1b02eea0f1757a283daaba05fa241b

--- frontend/src/hooks/auth/useSignin.ts
+++ frontend/src/hooks/auth/useSignin.ts
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:531b5a86deff6679abe38ea1db56b4baa1f628db222c5a24733cba5284fc654e
-size 236
+// 일단은 필요 없는 코드
+
+import { useState } from "react";
+
+const UseSingnin = () => {
+  const [signined, setSignined] = useState<boolean>(false);
+
+  return {
+    setSignined,
+    signined,
+  };
+};
+
+export default UseSingnin;
