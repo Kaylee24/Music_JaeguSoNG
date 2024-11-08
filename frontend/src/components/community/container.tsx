35e008254492383b92fff920c051cc45ff2d4e3b
박수빈 <tns027@naver.com>
2024-11-08 15:11:17 +0900

    FE : 커뮤니티 디테일 페이지 생성 중
    Former-commit-id: d0182782c4f5ba20e0f9e001e697bce05467f94d

--- frontend/src/components/community/container.tsx
+++ frontend/src/components/community/container.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43e0eb361f381dd87a3f4bf69d7bca496f58af96db14be6bfb52315e19e2e976
-size 550
+oid sha256:a1953c6dee231171b685017cbd37cae51dcc7da319c3e99313db00c8219ca39e
+size 902
