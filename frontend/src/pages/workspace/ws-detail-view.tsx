cd7e0c1b7ed8dd60d5be128c408f15fad87bee56
김송희 <kkbuh2@gmail.com>
2024-11-09 15:47:44 +0900

    [FE] 워크스페이스 세션 추가 api - 프론트 작업 중 백엔드 대기
    Former-commit-id: 015c91ea8a3195fd3fce4c0895cd439e33b52a9c

--- frontend/src/pages/workspace/ws-detail-view.tsx
+++ frontend/src/pages/workspace/ws-detail-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e1dfde08bcf3ceabdf70b4333ec80be1c662ed1c1ef6224b99f7421b13d2496
-size 1559
+oid sha256:d39285727d522ed1841a97da2327caa8f7a91720de54051051892dae0cc5e968
+size 1587
--- frontend/src/sections/workspace/sessionUploadButton.tsx
+++ frontend/src/sections/workspace/sessionUploadButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8adf2f932da64f2f8c636921ed7877bce9206714ce623abc4f8faacc0f5acfa
-size 3647
+oid sha256:a9e6b99d8a8b8e8394db0b16e8bc229435b6c4b764b683fff697a82270d77bc9
+size 4275
