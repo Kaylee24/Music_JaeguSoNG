340a7f0a18552dd43eb6da730fe6a5361296d1c9
송도언 <ehdjs9583@naver.com>
2024-11-10 15:50:54 +0900

    [BE] 워크스페이스 컨트롤러 consumes 제거
    Former-commit-id: ae069294603a6b5e96c267e15da684a136dcab1d

--- src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e71841fe182935e18d166cc3098c0608b4142e767b2921c65df1d74ec2d6a66
-size 5852
+oid sha256:5f6a7f8b36bfb3ada492ff6bec5a13c9b2a19097ecc96b929ba37d775c494584
+size 5818
