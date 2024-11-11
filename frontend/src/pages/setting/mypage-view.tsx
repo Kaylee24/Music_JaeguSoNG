9592f7e9b8b4393c7c8fa2659d7a2a45cac5728c
박수빈 <subin57419@gmail.com>
2024-11-11 16:45:40 +0900

    크루 커서 효과 생성
    Former-commit-id: 6d5656852cfd8131098bb17eecfda2f3a4f11b83

--- frontend/src/pages/setting/mypage-view.tsx
+++ frontend/src/pages/setting/mypage-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3d7c2d92bf8f43562d4924b432b2528b5027809cf5f78d6df9bc61a1f420253
-size 9428
+oid sha256:41777ef9ae1d0e5c1db25a9d6789bb590f1bb46f91af6e09d9f7544875a2c82d
+size 9313
