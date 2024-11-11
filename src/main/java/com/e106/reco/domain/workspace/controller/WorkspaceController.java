16412703aaf0c5377eeeea5fd8aacf0b423ebad0
송도언 <ehdjs9583@naver.com>
2024-11-11 11:04:17 +0900

    [BE] 워크스페이스 컨트롤러 url 수정
    Former-commit-id: 22e33f231387065da82a8d2a8957d7312981f6b0

--- src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:931e5dde1fb8fc9489179ec381ea8e5e629eb35d3582fdd91a5d227903a67700
-size 6181
+oid sha256:1966a95aa8991dd58370bf114956d190839223d350c04a2ffba41c9a2dcd815d
+size 6209
