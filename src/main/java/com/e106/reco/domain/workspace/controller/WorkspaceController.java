634d8c18804171826a7b260e75d78ac2dc314538
Songdoeon <ehdjs9583@naver.com>
2024-11-11 10:38:50 +0900

    [BE] FCM 알림 개발
    Former-commit-id: e4d0fda9426b974e594db44920456e982121c840

--- src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d164047b9ef94e86daad343852bfd1c1a7582f3be16d463e0e5c82c1c6233f2
-size 5662
+oid sha256:931e5dde1fb8fc9489179ec381ea8e5e629eb35d3582fdd91a5d227903a67700
+size 6181
---
+++ src/main/java/com/e106/reco/global/config/FirebaseConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f1597a379946d5601d082fca2d4629fda8e1bca58f102fa1b0d649bb1a1c8b5b
+size 1283
