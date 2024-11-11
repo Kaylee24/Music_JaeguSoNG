d27d3c1a5451d353d06a7e680158d591e16c5373
kkbuh2 <kkbuh2@gmail.com>
2024-11-11 11:27:15 +0900

    [FE]워크스페이스 포크 api
    Former-commit-id: 7fa2d96e9225dde230d32b907617cd86f2469727

--- frontend/src/pages/workspace/ws-detail-view.tsx
+++ frontend/src/pages/workspace/ws-detail-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab3682217863927b8f02748afae7f661e50188aa544c7dff7f784dcc51767af3
-size 2638
+oid sha256:2d764fba363c90df6c790dd41e36c33e5d460007f6a87998ef3b1fcca7fba5ed
+size 2414
--- frontend/src/sections/workspace/sessionUploadButton.tsx
+++ frontend/src/sections/workspace/sessionUploadButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9e6b99d8a8b8e8394db0b16e8bc229435b6c4b764b683fff697a82270d77bc9
-size 4275
+oid sha256:88fa97b815df69cbac860e12ac9554a0fe78f270d1c75f2314979fa6645e0ffc
+size 4482
