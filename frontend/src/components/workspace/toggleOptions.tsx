a7813e54e71366cbf63bb15cecd1cb5b13b84ef4
김송희 <kkbuh2@gmail.com>
2024-11-12 12:28:40 +0900

    [FE] 디바이더 업로드
    Former-commit-id: e8ea4378ff5d9eda5c41182860cbc2dfcd634ef7

--- frontend/src/components/workspace/toggleOptions.tsx
+++ frontend/src/components/workspace/toggleOptions.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22a84f6837995e66604c1cef4f5af7e9916ebc5594c43d4fecd3b338e158a2e6
-size 1334
+oid sha256:eefbd552ca574cffda69aa5640cbde5d47b4030c09b71ac64857c7523b40dad0
+size 1494
