fb7604e43d4e86d5ac8eccb74f34ea68504baf52
권혜경 <skygprud0707@gmail.com>
2024-11-12 17:59:27 +0900

    Be chat
    Former-commit-id: 84b50a3ca184619374ee78ab3a3d42d286356d3b

--- src/main/java/com/e106/reco/global/config/RedisConfig.java
+++ src/main/java/com/e106/reco/global/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e70365ac8036044e68b8b9eb6c7f920b149921798cd7dc243ac1ae770f6ff76
-size 2765
+oid sha256:18f52d53702c93a2fd4d0a1c4df597d4cbdd25b573a76ea4c23a3f1d6cc1fcfd
+size 4811
