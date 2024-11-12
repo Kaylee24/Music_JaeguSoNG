597a202be0c262ae65765a89643dc11530b6c0b0
박수빈 <tns027@naver.com>
2024-11-12 16:06:17 +0900

    FE : 검색
    Former-commit-id: edd96d560699be09ae85bbe303f9edfdbd286c45

--- frontend/src/pages/community/community-detail-view.tsx
+++ frontend/src/pages/community/community-detail-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b40dbb76631e4bfb3683d2df39363785fd643ddbdf13fa088df202eb6c5fa67
-size 6486
+oid sha256:229368c930ff0f80c72789da93679c9fc1839e4480040c0c1270db2a8b749041
+size 7036
