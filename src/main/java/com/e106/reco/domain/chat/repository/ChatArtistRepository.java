d857776e36af422f1f08894bca27ecd1213d8ea7
권혜경 <skygprud0707@gmail.com>
2024-11-12 20:37:01 +0900

    feat : board
    Former-commit-id: f6e27ee59ffca45daf2569cc1fed91e1c9f2c022

--- src/main/java/com/e106/reco/domain/chat/repository/ChatArtistRepository.java
+++ src/main/java/com/e106/reco/domain/chat/repository/ChatArtistRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3f14dc7f1671e58e7142364b210746e8956002d0ebbf4ca4f8cd6f6b1b0dd9a
-size 1695
+oid sha256:99fb5692f0b549c14fa76e7ab6d4d6a0a1cbd60268f014cc900cc6b78f8749e4
+size 1795
