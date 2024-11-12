38cf44de2063491a5f349872dd401560be25a22c
구현우 <kjklovekhw@naver.com>
2024-11-12 17:31:24 +0900

    Frontend community hw
    Former-commit-id: c615c16e5f8e50a8d3f000b59e90cf6456667159

--- frontend/src/components/community/review-container.tsx
+++ frontend/src/components/community/review-container.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b6ffb7c2c2750b7fc331a59a6013d71539c0a5687700e3720ed1dc8eb267cd3
-size 643
+oid sha256:206cbb6e2ee7f3fa7298e5428bebfb41143c47ab035d169459fad60e30a51bb1
+size 5598
