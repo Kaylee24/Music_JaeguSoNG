da40bccfde0f373dda0e50407809dc4c544f6086
김송희 <kkbuh2@gmail.com>
2024-11-12 15:19:56 +0900

    [FE] 워크스페이스 리스트 UI + favicon
    Former-commit-id: 754bca3b7bf0ee35a5b3e97e8815158f9063d148

--- frontend/src/sections/workspace/cardList.tsx
+++ frontend/src/sections/workspace/cardList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fedb57792539c92937ea00bae376f743783d295b9552760e57c65aa1feda45d9
-size 5680
+oid sha256:4997431e1b5a89fd6bffa8012f05c65084050d43fc583431824db2f2a11a8a68
+size 5750
