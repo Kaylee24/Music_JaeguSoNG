a0b1b5c635fd598059a9bdf096c81b53da9f82b1
권혜경 <skygprud0707@gmail.com>
2024-11-12 11:24:09 +0900

    feat : redis 수정
    Former-commit-id: 5ee439d3c7633d2646e1a4e0675dcb9320a424e6

--- src/main/java/com/e106/reco/global/config/RedisConfig.java
+++ src/main/java/com/e106/reco/global/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bf0bdfd97b1662f5d878cb8fe09078f977502c5531b1ea3b10bca4b625553cc
-size 1527
+oid sha256:79eb53ae4acfb4484a678081272672379fddf9d471637187d557b82cdecc194b
+size 2765
