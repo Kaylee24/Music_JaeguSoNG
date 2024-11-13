d955f673236b5b7d31e97c6cd4cca78a4c3f3e33
송도언 <ehdjs9583@naver.com>
2024-11-13 16:05:55 +0900

    [BE] config 수정
    Former-commit-id: e6ff10af82c9c79a70b924598144ec630978f7f4

--- src/main/java/com/e106/reco/global/config/WebFluxSecurityConfig.java
+++ src/main/java/com/e106/reco/global/config/WebFluxSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b69c5df18a7896237a1496d60fa016af3c0b518cdd95c6aa525b8565d647fe9a
+oid sha256:45b876bdad0bf1b5e363811afb270e3c115a863b9b781fc4e242f14949d0df7b
 size 1744
