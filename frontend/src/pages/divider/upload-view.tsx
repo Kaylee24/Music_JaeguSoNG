30efee0f9351d2d8457daaad2be8fd28eba7e6ca
김송희 <kkbuh2@gmail.com>
2024-11-13 17:45:53 +0900

    [FE] refactor: upload-view.tsx
    Former-commit-id: 36d854cb4c899b1cfd35bd1d8295c451f7f2095f

--- frontend/src/pages/divider/upload-view.tsx
+++ frontend/src/pages/divider/upload-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d29b02c134f55a8e7896e575130431f41b5ac5836c2007a79f6e5666e9d230b5
-size 7757
+oid sha256:1d81205ea95e8cf004c5221acad580cf710ae76d8b54f6ab7cf55a3125dd4bbb
+size 2486
