55bd571f39243a228cf896fba4520dcf4b07657a
김송희 <kkbuh2@gmail.com>
2024-11-15 01:15:39 +0900

    [FE] 워크스페이스 디테일 썸네일 제거
    Former-commit-id: e7f347917a358747caf5da15424f41f56f8f44d2

--- frontend/src/components/workspace/session.tsx
+++ frontend/src/components/workspace/session.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67d804ed29b4832249e4c240313c58f0db752063eec79a6a52bd134c5fbc4e98
-size 12352
+oid sha256:612ad791cf08c91e15c9232d4c944a2e6db334dda7e04ece55ed7b5b341fde0e
+size 13276
