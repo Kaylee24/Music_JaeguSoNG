bf6a4817605d7c8962fc4405bfaea2ea042f6d40
김송희 <kkbuh2@gmail.com>
2024-11-14 11:35:52 +0900

    [FE] 워크스페이스 디테일 페이지 1차 구현
    Former-commit-id: 362f0912808e7e8dd9946b368ce3afc78bffa95a

--- frontend/src/components/workspace/session.tsx
+++ frontend/src/components/workspace/session.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9911bae5640de4064cc7a1457b6a0e6e430d490d9050fdc95d1a027b240693a6
-size 6007
+oid sha256:67d804ed29b4832249e4c240313c58f0db752063eec79a6a52bd134c5fbc4e98
+size 12352
