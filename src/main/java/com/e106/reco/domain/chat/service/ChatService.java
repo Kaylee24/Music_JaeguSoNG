ac414058e3c2cea723dea5c4c8c5ab7fecd4e259
KHG <skygprud0707@gmail.com>
2024-11-14 12:28:16 +0900

    feat : cors
    Former-commit-id: 393563320e6a2d84f753b6386852538c863ee4d2

--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51b1c78ce6b9555eb79c45e02bb6eb0e86699409a919fff5ce7b046fc0ca1ecc
-size 12491
+oid sha256:0e669a73a31a7a29c8cd082198a378be3b1cf5f13d82b35aaab03118db03217a
+size 12393
