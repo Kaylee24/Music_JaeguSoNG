1293669605ef5df39597ef05876f3a33b9e10cf5
Songdoeon <ehdjs9583@naver.com>
2024-11-15 15:52:38 +0900

    [BE] recommendRepo artistSeq로 변경
    Former-commit-id: c376741a825719b417bf1cdc6fd25e7bd91922d9

--- src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
+++ src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e64463e1cd2211c721420b3bbfe668d19ac84c503d4486f8fec771b939187c1d
-size 5455
+oid sha256:ecedde48da84948681044d0fe1295b9e02d365c39ba54657a7b5d18e99206cbd
+size 5611
