b024db703fdb61967e295ab3c715b1a2253babc8
구현우 <kjklovekhw@gmail.com>
2024-11-15 15:47:04 +0900

    [FE] feat : 채팅 창 크기 수정
    Former-commit-id: 1c8ea6cb05366140c784f8bb91e3a18320bcf5c2

--- frontend/src/components/chat/chatlist.tsx
+++ frontend/src/components/chat/chatlist.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63cee27822b7d6d916a8854848d55f8ba8dc4e68c399eec0782d15534fdb338b
-size 18884
+oid sha256:6d70e0411b378bb70343fee5cec8e0c479b1bb73b125f94fa42348d9dcdbadbe
+size 18853
