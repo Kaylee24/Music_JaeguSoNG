908f63d02ab6bf1d4ece8797e57505a54da1c97c
구현우 <kjklovekhw@gmail.com>
2024-11-15 15:44:25 +0900

    [FE] feat : 프로바이더 설정
    Former-commit-id: b5a0ba53b96c747a217ecdb06753b4ac243ce9a3

--- frontend/src/components/chat/chatlist.tsx
+++ frontend/src/components/chat/chatlist.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2bbcd097f8dbb102a6b5bd64c923e982f4d8ce30daefab02840d757002cfd1b
-size 18739
+oid sha256:63cee27822b7d6d916a8854848d55f8ba8dc4e68c399eec0782d15534fdb338b
+size 18884
--- frontend/src/components/ui/provider.tsx
+++ frontend/src/components/ui/provider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3147cef480023e51b71c2ffec06af8bf88a3d0671c0a6f0b1d09d0f9d31e746c
-size 395
+oid sha256:a622696f6e29118ae2f7814cd273115504e453a3464147b9d730e0a60e0da0f9
+size 647
