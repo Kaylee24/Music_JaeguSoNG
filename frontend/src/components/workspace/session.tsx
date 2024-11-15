cf340b58b6b88ee48e28762a9284f2f6a527bc02
김송희 <kkbuh2@gmail.com>
2024-11-15 15:37:33 +0900

    [FE] 스크롤바
    Former-commit-id: 3c3ebba5279c405f9577eba0c2e030b8c166ec22

--- frontend/src/components/workspace/session.tsx
+++ frontend/src/components/workspace/session.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cfab63e75624d2e0628fd2ed08923d1ac86f266bfa1dae85ffb09fa1776df30
-size 15202
+oid sha256:1d99886506e9312d5a292c8411887166de2d4c21d15809828f6a3b2a072e2eed
+size 15164
