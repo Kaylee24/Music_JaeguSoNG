dea2dcb4b9cff4eff6f872a5d30f0cc4d479a826
김송희 <kkbuh2@gmail.com>
2024-11-15 16:57:24 +0900

    [FE] 워크스페이스 기타 등등
    Former-commit-id: 1d835da22974fd0abcdc3ba84cb6189abcfb294f

--- frontend/src/components/workspace/forkButton.tsx
+++ frontend/src/components/workspace/forkButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7bcc1bfe8cfab8c14eb1b96c3d7eeb29305e128b0d4879faa5132ccc56ad499
-size 1099
+oid sha256:327e973b1e7fd1913f62ee8ad73ca5a2bbba1ce01cbc7b7ddaffd9c94b3e55f3
+size 1570
--- frontend/src/sections/workspace/sessionBox.tsx
+++ frontend/src/sections/workspace/sessionBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11544be81407452f5b3c79e33dad69620723ef2efe67737ed69ac14939e910a3
-size 1857
+oid sha256:5ca2e117b8a2ae489398409df3c7e663da04326069caf800c636509c3ea3519c
+size 1850
