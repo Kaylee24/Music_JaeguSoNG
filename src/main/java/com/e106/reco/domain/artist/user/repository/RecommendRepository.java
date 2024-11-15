4b8f185c80de75162af552ce89102f6bab8b9d9f
Songdoeon <ehdjs9583@naver.com>
2024-11-15 15:57:40 +0900

    [BE] artistName 수정
    Former-commit-id: 692a172a90ebd16d455a5b720542e8cbfe8b7dc4

--- src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
+++ src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fff03809800913ea439e1d649b400f9480abfbb2b7e456454a8594cdbe4078c
-size 5469
+oid sha256:8e3a3a22e0e87ca96f4a48ccb5dbd77f4f68325a6bee3eb40fb2ec23965e9e68
+size 5440
