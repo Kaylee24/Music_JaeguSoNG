b8d76aa6740a18adf08b01a00a166f52bac40d14
Songdoeon <ehdjs9583@naver.com>
2024-11-15 17:52:39 +0900

    [BE] follow target_seq 칼럼명 변경
    Former-commit-id: 00558d7bdc96c99dac9463f8a5750ba2f1c37e39

--- src/main/java/com/e106/reco/domain/artist/follow/entity/Follow.java
+++ src/main/java/com/e106/reco/domain/artist/follow/entity/Follow.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd8a765e21ffc096d444814ec180d108557aefa80586c0c42271f3c49d6fa7b4
-size 1940
+oid sha256:b1c57fa35abb9946f5dac717a29e30482786b5e84e62fe05640651062add2b60
+size 1943
