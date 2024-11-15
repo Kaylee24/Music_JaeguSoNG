525c1de47568e1fe3616bbee36607a9cc0b4f8fc
KHG <skygprud0707@gmail.com>
2024-11-15 17:58:18 +0900

    feat : 유저 정보 내림
    Former-commit-id: 41b136d00e8bc084422978a499bfd32c6c5ab50f

--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e977b636c48bc2eda83dee81728ac24fd61fb935c32f259e6aa65daa05e2636f
-size 13268
+oid sha256:e7ac5dca6436aa3cb91a6b8072ccb5cbd942d39f275ab28dab719336e1fe6ed5
+size 15776
