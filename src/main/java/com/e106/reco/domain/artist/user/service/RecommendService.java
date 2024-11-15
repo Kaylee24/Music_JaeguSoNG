85708fc749716e6e6c0b4861c8eac2a858b01094
Songdoeon <ehdjs9583@naver.com>
2024-11-15 14:46:46 +0900

    [BE] 워크스페이스 detail 수정
    Former-commit-id: e9e4990d2f67902d600b3e6c3c6d17d08de4f08a

--- src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
+++ src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27d3e7c22dad0ca5905e9c3c782d67a7281a25f4e1cd9fe4f93f3ed6e7174fd1
-size 5049
+oid sha256:b38e54e4976cee9ac97655e8860c889c5e2e3efeb22d158078d93ab506af66f0
+size 5083
