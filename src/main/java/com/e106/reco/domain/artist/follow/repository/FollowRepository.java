128b95917fbdd6705e25bef61751fc4c908b442e
Songdoeon <ehdjs9583@naver.com>
2024-11-15 17:54:25 +0900

    [BE] 쿼리 수정
    Former-commit-id: a077d820fcd147a0c85b003fc8f355f79c17506a

--- src/main/java/com/e106/reco/domain/artist/follow/repository/FollowRepository.java
+++ src/main/java/com/e106/reco/domain/artist/follow/repository/FollowRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1dcfcdb0905ab6679c1ce762da4d3cb2e151e4b2ee3adff3a4321ebb7e3a15c
-size 1039
+oid sha256:a39a8ac3863ee032f113b683f38b8b57f3756c79c0cebb043d8c7139eae2cfe2
+size 1037
