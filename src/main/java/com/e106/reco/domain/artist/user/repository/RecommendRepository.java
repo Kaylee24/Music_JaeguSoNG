5c1421029f50e174c5e507616d31662ca1fc6bac
Songdoeon <ehdjs9583@naver.com>
2024-11-15 15:20:42 +0900

    [BE] recommend param 추가
    Former-commit-id: 0cb0fc362666372b321ff2b64372f2d381c7018d

--- src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
+++ src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0db0681b1011b40582aa7eee2b1137709f4a20f8ba0eef952a22f3e03ed4da0
-size 5765
+oid sha256:f0111d60dc7c054dafaef573a61bde0aeef9133c6d78dfac3f6431a308f952a5
+size 5515
