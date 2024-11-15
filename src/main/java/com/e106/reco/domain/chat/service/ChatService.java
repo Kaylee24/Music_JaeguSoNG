f8d47e7166cc04203ccc14dc3303856005e83bd7
KHG <skygprud0707@gmail.com>
2024-11-15 15:56:03 +0900

    feat : 가입일자 기반 채팅 데이터 받아오기
    Former-commit-id: 31293c07f4a8ec18f7c6d52f30dad94c9bc833cc

--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9b25a0940fe23cf150902c2c79222698bfe9daa948fd3704c2c224a817d738a
+oid sha256:873986def05ac839bd9d8060daed77d53f57cff287501014c2225424aa7f001e
 size 13422
