dd540b1ee94edfa5dbee5ffda87d81745cf99dc3
구현우 <kjklovekhw@naver.com>
2024-11-15 14:30:42 +0900

    [FE] feat : 채팅
    Former-commit-id: 89d5ada14fcd78ac6207c4c36335aa2b6125dc44

--- frontend/src/components/community/otherHeader.tsx
+++ frontend/src/components/community/otherHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c738d0e1c697446e12f9d2ee2dd5984a35e8cb26c9914ca97314fe6ea00a73f
-size 6237
+oid sha256:2cb07617edf093e2d5bd8b1935bb5a68392cf89d4338e1b21bd9bd9968563283
+size 2837
