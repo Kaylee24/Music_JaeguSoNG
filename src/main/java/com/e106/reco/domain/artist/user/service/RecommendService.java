76ed2ff4635cac8804a872c9e4423e3dc9210d9e
Songdoeon <ehdjs9583@naver.com>
2024-11-15 16:01:38 +0900

    [BE] artistName -> name 변경
    Former-commit-id: 974c6484547fd3bff036e8561acbc4f7bb583bc1

--- src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
+++ src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d3e4d5975758e32475677ebc747af68e33098c8e9d65cde8c558a387a28d371
-size 4969
+oid sha256:4db7fef7eda4faf2514a907ed33175ea16ae3c7dd8a4754961bac04a55d7d579
+size 4963
