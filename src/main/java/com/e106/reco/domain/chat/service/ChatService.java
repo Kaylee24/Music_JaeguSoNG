c09023108d2b3c1c02f260aefe20613d7e4b46e2
KHG <skygprud0707@gmail.com>
2024-11-15 21:01:26 +0900

    feat : 채팅정보 가져오기
    Former-commit-id: dfb262903637f80b45609f4542f0bf51ef973306

--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e05c542233c579363fb8e7cbe7e3ce5b53c40689d09a1ea3cdce93e28259083
-size 15127
+oid sha256:dc970d3eeca4227ade7b49f130b1800e50f91258205ec19894e1d77f8acb7938
+size 15196
