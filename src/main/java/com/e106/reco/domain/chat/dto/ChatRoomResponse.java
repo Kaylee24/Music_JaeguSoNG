ef72682d19a4c8e6d794d047043efd7751956867
KHG <skygprud0707@gmail.com>
2024-11-15 12:30:59 +0900

    feat : 유저 정보 추가
    Former-commit-id: 1b0bafab6a56459c4354dca3f90ae80f6fed32f3

--- src/main/java/com/e106/reco/domain/chat/dto/ChatRoomResponse.java
+++ src/main/java/com/e106/reco/domain/chat/dto/ChatRoomResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bb9bbb4a51d95c7b15c074e515b858e067628fb4f20f2b9caecdcb724486334
-size 516
+oid sha256:2977406cfb33cd80d8be044f331f1ab4ffaec823afd32cb0fd4e3d4225218d6a
+size 578
