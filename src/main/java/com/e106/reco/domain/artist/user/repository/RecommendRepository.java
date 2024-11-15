0938da7c5ad6022a6c747aa115baa458582c1616
Songdoeon <ehdjs9583@naver.com>
2024-11-15 15:11:17 +0900

    [BE] recommend Repository 수정
    Former-commit-id: 47e1ebbc3a067070cb5ceb9ca4df46dba8305b3b

--- src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
+++ src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e30cf5d27ca45d317d806f1b9f00a7b318f20b881310906b96632ffb8d974c6
-size 5717
+oid sha256:24638fceec82269b13c19b8a09d4d08837a5c7fba92be18c1dfc680b7e123097
+size 5624
