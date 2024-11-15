db1ec0259d4d1360f0551ee5d66670a94584d5b1
Songdoeon <ehdjs9583@naver.com>
2024-11-15 14:47:36 +0900

    [BE] 레포지토리 추가
    Former-commit-id: 2eb2b38e9a49593013bd75c85b109bdf23844195

--- src/main/java/com/e106/reco/domain/workspace/repository/WorkspaceRepository.java
+++ src/main/java/com/e106/reco/domain/workspace/repository/WorkspaceRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81cfcbb6e73f022529b8d76cfe90cc71cd8358a84f543f92c6a4e2b0a31043b1
-size 684
+oid sha256:55f595bf6200868eaacddd5623a192598430126402c01593db5e949f447db032
+size 880
