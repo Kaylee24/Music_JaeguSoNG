6d25882a86a258a877cacaa115ef6b4491438473
Kaylee <kkbuh2@gmail.com>
2024-11-17 04:25:20 +0900

    [FE] 워크스페이스 상태관리 store, 기타 등등
    Former-commit-id: c6b884dd74131e3eed9776dafc4282e9d8fec162

--- frontend/src/sections/workspace/play.tsx
+++ frontend/src/sections/workspace/play.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d41cb46c14525ca157459f39437e32080c8a25930679ae0c58cbb15a4b36987f
-size 2624
+oid sha256:1c134d27d9349b696cd27fd1c309b9d454f5fc6000aea3fa8a4f03ace3b91619
+size 2236
