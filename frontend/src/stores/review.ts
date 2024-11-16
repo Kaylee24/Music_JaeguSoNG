06e522a383c70b59b865d6d690efa1458734bf52
Kaylee <kkbuh2@gmail.com>
2024-11-17 05:31:10 +0900

    [FE] 워크스페이스 point POST 요청 / 500 / requestBody
    Former-commit-id: 34f47304d874cea69d387e03c0ded1f4a58f1e0f

--- frontend/src/stores/review.ts
+++ frontend/src/stores/review.ts
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c8bafcd09fe11e75bd830c0e9587e7d8ebd6bb743c30598cfc0844dd0339ccb5
-size 312
+import { create } from "zustand";
+
+interface ReviewState {
+    changeReview: boolean;
+    setChangeReview: (value: boolean) => void;
+}
+
+const useReviewStore = create<ReviewState>((set) => ({
+    changeReview: false,
+    setChangeReview: (value) => set({ changeReview: value })
+}))
+
+export default useReviewStore;
