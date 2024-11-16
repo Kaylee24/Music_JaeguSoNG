d6c0c7e0104ea7ca858f266213434a72c1af3c3d
김송희 <kkbuh2@gmail.com>
2024-11-17 05:13:49 +0900

    Update WorkspaceService.java / modifyPoint
    Former-commit-id: 2f7060fc9652148c25252dd8d14b12983ed8c2b7

--- src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e58b92e9a88ebe128f11f9f21c3319c0439f0b10aaffafd704d8e09296e1317
-size 6319
+oid sha256:ab73a4e61900425b30be28929d4073a6ada07b68e142488daed412315238cdc1
+size 6445
