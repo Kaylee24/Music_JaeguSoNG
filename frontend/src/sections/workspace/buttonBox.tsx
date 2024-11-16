9f590c10817e3fc0b02408f41938a3fc29b3c2ab
Kaylee <kkbuh2@gmail.com>
2024-11-17 06:13:41 +0900

    [FE] VIEWER 글로벌 재생 플레이어 fix
    Former-commit-id: 58c0bca5d4708ebe436a937f5a5c45a68f52fe85

--- frontend/src/sections/workspace/buttonBox.tsx
+++ frontend/src/sections/workspace/buttonBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bda7cb6cc80aa9ecb271bd854b21f10d88161bb355a1e0f7ee8767114604f3e
-size 1100
+oid sha256:1e9463a4e8e77785b5068826f995f064afc08c258c8a786f1e28623b2daadeff
+size 1240
