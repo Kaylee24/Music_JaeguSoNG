956aacf5962b67d05bc495b4ef3d9c90298dd4bb
Songdoeon <ehdjs9583@naver.com>
2024-11-17 18:09:48 +0900

    [BE] divider 스타드
    Former-commit-id: b075b95171da8c683f54297f6038c7f37373ea4e

--- src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
+++ src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f10b052f360ed49e4dd274e3a0666821b4dafdbcb34c073fd9af0e9a95657a5
-size 16945
+oid sha256:e110ad88c628d808d83d16014dcb93b1465201be9b27c44bba9b51280a1d06f9
+size 16983
