e96a36b489a182b7abb3c0f7c0adccf23dc987ad
Songdoeon <ehdjs9583@naver.com>
2024-11-17 21:37:39 +0900

    [BE] jpaTransaction 선언
    Former-commit-id: 805ab2a3a2da378352a3c52c7d99e90b84b195f5

--- src/main/java/com/e106/reco/global/config/Neo4jConfig.java
+++ src/main/java/com/e106/reco/global/config/Neo4jConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:048864ce5fb28246df7afd200dc8f5e532425c551cf2b32e0c9130d6521b250c
-size 500
+oid sha256:1a608d3b451660755d85e6dd12a315491a06d17c0dbf1817621246bb4d2ee803
+size 1168
