c7c4c33f0dad55ecdcf4c5e3e694169fc57e9ccf
Songdoeon <ehdjs9583@naver.com>
2024-11-17 16:28:11 +0900

    [BE] 파라미터 명 변경
    Former-commit-id: ba826517612bcc474c778730faf08f894e903847

--- src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/com/e106/reco/domain/workspace/controller/WorkspaceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2559463e9e176e2d99ffd073fc7e8db8d33d7926c9ddf5285e5ee9635c6cbb5
-size 6421
+oid sha256:7706f018d0508f859d281d4fd1ed913159735b14752a4c66bd00a9e163e49d17
+size 6427
