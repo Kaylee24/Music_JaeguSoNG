b29f09ae39c27cb2439556661e0c7e356a3fe16c
Songdoeon <ehdjs9583@naver.com>
2024-11-17 21:23:11 +0900

    [BE] 트랜잭션 매니저 빈 네임 변경
    Former-commit-id: 0f2a395abad03587415051bb31ee19330c7d086e

--- src/main/java/com/e106/reco/global/auth/service/AuthService.java
+++ src/main/java/com/e106/reco/global/auth/service/AuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0a72560e9b920be1362f13771d69a680d0d2129abd4b1ec3d593e55c213e7c1
-size 10960
+oid sha256:7e7bb1d8d28b3b7ae3223ee3c250fff0485dd133775c395b2465a2d4526e6229
+size 9920
--- src/main/java/com/e106/reco/global/config/Neo4jConfig.java
+++ src/main/java/com/e106/reco/global/config/Neo4jConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39461539579cebeb23cabfac4860b3bc50ea3ed5b86afca2d742348ac1f566e6
-size 689
+oid sha256:ab809fd9a87e40c00b9548aaf7fe4b474b60a8b997664a4a84a3729afcc0880c
+size 709
