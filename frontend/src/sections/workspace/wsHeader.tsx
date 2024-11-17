2fbe9c2c9a15ee28b4a1a9b7083e9bc7336ef6fe
김송희 <kkbuh2@gmail.com>
2024-11-17 17:10:15 +0900

    [FE] 워크스페이스 fork, partial save
    Former-commit-id: 624c8db2796e39041dd96272b55990867be6853a

--- frontend/src/sections/workspace/wsHeader.tsx
+++ frontend/src/sections/workspace/wsHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:861ff8088d5531a0d1062ea24f747ae8dfc2b43a997883fa35a6f756e0be1163
-size 7208
+oid sha256:c8782bec8a5fafbe3cc25b2af31608117e5b407a4e09e7ed7c1afa8063e83d5b
+size 9236
