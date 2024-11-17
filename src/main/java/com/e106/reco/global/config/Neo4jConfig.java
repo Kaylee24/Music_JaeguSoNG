db7fafb1fd1c36acde208a9c15da65e4ae247965
Songdoeon <ehdjs9583@naver.com>
2024-11-17 21:27:43 +0900

    [BE] neo4jTransactionManager bean 네임
    Former-commit-id: 305fc8ff1bbf2d815f5bdf0eb2c77d1989fcc1e8

--- src/main/java/com/e106/reco/global/config/Neo4jConfig.java
+++ src/main/java/com/e106/reco/global/config/Neo4jConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15fcdf3ba68118b42088402cd407f8bf035011d3df3356c5053f8c155d573ebf
-size 729
+oid sha256:048864ce5fb28246df7afd200dc8f5e532425c551cf2b32e0c9130d6521b250c
+size 500
