b2afb2961423d56e4699c786f276020f6e56ee5f
Kaylee <kkbuh2@gmail.com>
2024-11-17 17:46:33 +0900

    [FE] 워크스페이스 fork api 수정, 디바이더 등
    Former-commit-id: eec41220b8482e225bf697e98641529db12ae569

--- frontend/src/pages/divider/upload-view.tsx
+++ frontend/src/pages/divider/upload-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d81205ea95e8cf004c5221acad580cf710ae76d8b54f6ab7cf55a3125dd4bbb
-size 2486
+oid sha256:2bc1d1b1e9bb1375f7bf25e129b238a1cf1d73776dfd575470c049dad7c73d30
+size 3368
