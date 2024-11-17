1541e38fb0b40cb7c16063ad4f97852a472cc2a8
Songdoeon <ehdjs9583@naver.com>
2024-11-17 10:45:39 +0900

    [BE] 로그 추가
    Former-commit-id: 9a9707544b72f23156d3115c7eae2bb72caa4393

--- src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
+++ src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf5acf4357b171263f3acb6ca865631342f097195a76239f70157552a180e1cf
-size 6038
+oid sha256:ecf7e23e8948c09a5a27bade365ee5e4cc2d4418e0acd536fbba32b2e3b22e64
+size 6188
