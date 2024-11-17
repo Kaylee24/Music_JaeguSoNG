a9182494d9e5a8d36e1234efd57a1825228e1cb3
Songdoeon <ehdjs9583@naver.com>
2024-11-17 18:18:28 +0900

    [BE] create workspace 메서드 분리
    Former-commit-id: 60ca0d661fd92453d8690ccfe3b6a9777b2df3bd

--- src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
+++ src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bed62ba6207c6ba94becf4f2f5a9564be5906059aa94b55cccb604c7e62204c1
-size 16975
+oid sha256:44ad886594c85e7cbb94aef273fce234e5c57f64634507f9348eecfbf2aec0ea
+size 17130
