d7817b869d48a9a5fdd8ca52cc421faf81359ebe
Songdoeon <ehdjs9583@naver.com>
2024-11-17 11:01:00 +0900

    [BE] artist 복수형 제거.
    Former-commit-id: 45aed1dee366517976135643581c6f321c29f3b7

--- src/main/java/com/e106/reco/global/auth/service/AuthService.java
+++ src/main/java/com/e106/reco/global/auth/service/AuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f2e6ee5788b79d627c5b2ed0777eff7af5ffd70d973b7782b9179e281aaf7f8
-size 9713
+oid sha256:9be7ae255dbc971ca2fd014dcb35025cd362028560e66ee528d7c7844e8d2cc8
+size 9930
