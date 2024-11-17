88ce351399652fd28556ee020c4f6641622dde4b
KHG <skygprud0707@gmail.com>
2024-11-18 00:05:12 +0900

    feat : 채팅 목록 / 유저 리스트 / 내역
    Former-commit-id: f8785e3a03f502b21e8df8f06198f945ed3459e8

--- src/main/java/com/e106/reco/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/e106/reco/domain/chat/repository/ChatRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ade38466b4054ca447f5cb662757c3bede2c5e2ced87b3c22f78d0c107a9bae
-size 2504
+oid sha256:0d93e36366bfd2d3287af1e766920a43525475e3a30083569ecdf7ba159301d9
+size 2462
