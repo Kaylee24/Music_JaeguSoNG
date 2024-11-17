e524fb37a1095d3db87366404e6f195c1b4362bd
Songdoeon <ehdjs9583@naver.com>
2024-11-17 21:39:42 +0900

    [BE] jpa 제거
    Former-commit-id: e53cec62a6e1329dcec5cf44ce65dad17d6cb035

--- src/main/java/com/e106/reco/global/config/Neo4jConfig.java
+++ src/main/java/com/e106/reco/global/config/Neo4jConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a6bd8af6b92fad5c26f0fe1dd5231f101b232a47a1eff16f99aba7c7d176683
-size 1141
+oid sha256:0f20c28a4ca7f13bdb6a07925a60e7d88a336376f0ace45c337e5d807bdb992c
+size 1138
