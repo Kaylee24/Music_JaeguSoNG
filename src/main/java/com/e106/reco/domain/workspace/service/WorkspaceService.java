036e89c100b793b4434226a1c018db5e562cccd9
Songdoeon <ehdjs9583@naver.com>
2024-11-17 18:12:17 +0900

    [BE] saveAndFlush -> save 로 변경
    Former-commit-id: 03db31de36241a726598419ee5c9914b5ff0ff77

--- src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
+++ src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c8ddea8506686670c5aefc95d07a0935adbeebf46d64c1c549f72b631f5c6b2
-size 16588
+oid sha256:c91f8fe5d6a39fa08c52a8e4c9c590b19e7756b646eff11742e652c20f07def1
+size 16580
