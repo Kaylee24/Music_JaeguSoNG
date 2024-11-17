89197bef56ae2086421b21d3af0e5fea635c84a0
Songdoeon <ehdjs9583@naver.com>
2024-11-17 18:27:14 +0900

    [BE] 찐막;
    Former-commit-id: 2abe2f953268904023861b84ccea3f69947f5c70

--- src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
+++ src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2e18188299afd4747b3e8fb75606c8b91f312403297631d3422b3b9c5655110
-size 17138
+oid sha256:4fe6d2170e9b8dfb5d2ed42bec84fa4be00cf067733a5aaacb40c4b0fc0adb0b
+size 16911
