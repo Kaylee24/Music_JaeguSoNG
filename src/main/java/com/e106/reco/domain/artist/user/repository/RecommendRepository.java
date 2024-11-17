c9051c1bd485eaab32b94d55715930fc43b1775e
Songdoeon <ehdjs9583@naver.com>
2024-11-17 11:20:30 +0900

    [BE] 프로젝션 제거
    Former-commit-id: 8e0753a707a5e906a19085b93e10848a23976166

--- src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
+++ src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:228678e417471678585bf23e0b86aaee604c3ea20aa9384f467207c495d6c7a0
-size 5539
+oid sha256:89b4bd9d72490765ea29e36c89f9aebaf368a98a84714f06a3f98198ff32f339
+size 5507
--- src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
+++ src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57fbed195489a3e37a44efb4f7bc4a0eef98f24ac8043029b355d2f8a77cb388
-size 6325
+oid sha256:f8efd79b6a84c9ff10394ef5ca6499b1ed91b32d55fc0fbc782f0c79f80f5c73
+size 6381
