ef32277455630bd8db4a1e50a96d8f87ba3da285
Songdoeon <ehdjs9583@naver.com>
2024-11-17 10:48:30 +0900

    [BE] artistSeq 추가
    Former-commit-id: 22bca8392c806262f5e8bf0eb495484a559a88bb

--- src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
+++ src/main/java/com/e106/reco/domain/artist/user/repository/RecommendRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36f2fb70dc579d9f1e386e43b91d4f7b83bf6ce55f32a937f9d21d2e6435bc0a
-size 5391
+oid sha256:eae251e147a00bf0864240019edeee885e703de6c934313cfce44ad3dacc4e9e
+size 5402
