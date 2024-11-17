2f7b71595c5757a5416ef6900359581d77d7dfdb
Songdoeon <ehdjs9583@naver.com>
2024-11-17 16:52:35 +0900

    [BE] 워크스페이스 저장 수정
    Former-commit-id: 5ebb6f1e4bfbb09464db2aa2ad5ddf9560f99c6c

--- src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
+++ src/main/java/com/e106/reco/domain/workspace/service/WorkspaceService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dad298e903830192b4042c20b701bb7e684e241bcdfce5cf9958a6f06f83819c
-size 16477
+oid sha256:f4471ba3823afdbc104eca079d773c11a6a33ab6a6b66ef14d7f15e939a59b40
+size 16406
