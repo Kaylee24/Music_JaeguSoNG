dbf1c8d738213f61e9a2bb106cf78c00e323e602
Songdoeon <ehdjs9583@naver.com>
2024-11-17 13:28:44 +0900

    [BE] 추천 닉네임 추가.
    Former-commit-id: f3c97e1809d8db0db578735d4aef412487e1a735

--- src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
+++ src/main/java/com/e106/reco/domain/artist/user/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:347070c6c42cfebe8e1d0a6aa3b5d6a8ba60ad9f629f76f57e468ddd84ddb36d
-size 6247
+oid sha256:e702653d7adfecae4c072276935b051922534cddf7f29ce8e9d59389cb724922
+size 6295
