58b92f57bdea6782d81a0669ce3f8fe3711936a2
Songdoeon <ehdjs9583@naver.com>
2024-11-17 16:35:35 +0900

    [BE] jsonCreator 추가
    Former-commit-id: a513f7ff05615a1ad618411867149d74e7e72522

--- src/main/java/com/e106/reco/domain/workspace/entity/converter/SoundType.java
+++ src/main/java/com/e106/reco/domain/workspace/entity/converter/SoundType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd8a9dbc0b77698a0e7bea921ba9704f1335bf3b5c42cf4866e6f09442c8d2b6
-size 923
+oid sha256:681de6a6e61d006de7f28a2d01ee4a71c7bff1e2807332cf518f6abbbf420b8c
+size 1026
