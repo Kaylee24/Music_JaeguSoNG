c6b9ed5786b96a650633db6192f0ee4480c357d7
KHG <skygprud0707@gmail.com>
2024-11-19 05:50:45 +0900

    feat : 채팅시작시 다시 초대
    Former-commit-id: 0b41111559e9261281d3b630503e149cc16c7df0

--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bd88f9c2268af7564f636bf049d7dbf8bf97e1d4f3c5575ba204cfdd03e3660
-size 19342
+oid sha256:940a08dd54b80f109d844f671a42fb2c959ee0e2248633b75b84631f81b6864d
+size 19385
