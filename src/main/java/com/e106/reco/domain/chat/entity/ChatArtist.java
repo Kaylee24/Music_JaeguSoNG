899ee194aa2220247707dff758847f4f48d04b21
KHG <skygprud0707@gmail.com>
2024-11-19 03:21:36 +0900

    feat : null 설정 추가
    Former-commit-id: 8d5f0de4c81e091871ed7d7e0a191bc7c6cb5b50

--- src/main/java/com/e106/reco/domain/chat/entity/ChatArtist.java
+++ src/main/java/com/e106/reco/domain/chat/entity/ChatArtist.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:743ee0e2482fa9cdc480591e3ed991ab0e13b0c3ce2928d33da281632130877b
-size 1879
+oid sha256:e2ef54d2f023a04405ef6464bc8977d7f1447cc0b5dcf753079def3bb75d78c7
+size 1975
