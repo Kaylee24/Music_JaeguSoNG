4997b40f58e52f2bb184dcff4e5c20d73e04b607
Kaylee <kkbuh2@gmail.com>
2024-11-18 20:32:40 +0900

    [FE] 세션 파형 UI 디벨롭
    Former-commit-id: d27ed10eb5f1be394a93e58549fb6bccfbda4e1a

--- frontend/src/components/workspace/session.tsx
+++ frontend/src/components/workspace/session.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49ae9dc3291e3408efdbe73c335de7e267e33e4fa9755e35783dd4ebc8dcb2a5
-size 20916
+oid sha256:b3041774a22b0cec72f2ff8a6494d228c2f9d43328da71bd7c76e19775edaf28
+size 21258
