11ff59fbde5af343e93b7048b959d5e811089421
김송희 <kkbuh2@gmail.com>
2024-11-18 12:18:14 +0900

    [FE] 워크스페이스 전체 재생 기능 구현 완료
    Former-commit-id: 706cda40c20d5936d8a491157e8ccd8e1d5daeb5

--- frontend/src/components/workspace/session.tsx
+++ frontend/src/components/workspace/session.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f17f6c8bcb1913ae595faf3110674a44f17b1a7654e6927ed3264df3db50df1
-size 19665
+oid sha256:67e6da96177fd574dc5162fe8e1ad4829c0a7659699140a72f0b7b7b2539bd6c
+size 19507
--- frontend/src/sections/workspace/wsHeader.tsx
+++ frontend/src/sections/workspace/wsHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6b7ae31ee780765527159bc13ce5e2333aaf61129fdf9ec73ce1f21f9c79d60
-size 9402
+oid sha256:deb4b657b2c32c91b6b3b374ce738dbb7cbdd215dddcbc485a08cc82982b051f
+size 9237
