92ab8144dcf7b79d29fb59cb43efae17bcc30df7
김송희 <kkbuh2@gmail.com>
2024-11-18 11:30:03 +0900

    [FE] 워크스페이스 링크
    Former-commit-id: 45fbd01661fb6dbba389a8c796b4af6d7759fbc0

--- frontend/src/sections/title.tsx
+++ frontend/src/sections/title.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aeb45e3548886b2e3d0323b083799bb05381b9cf629cca2dda0079ebb984cf72
-size 599
+oid sha256:f49b6b29ec605b8ecab5b2b3bbdf3e0c0398d63292315f8d45835f171feb367a
+size 1522
