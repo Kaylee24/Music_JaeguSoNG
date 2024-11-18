4ddecce142d869858b1eb62def5db3b4451d3adc
KHG <skygprud0707@gmail.com>
2024-11-19 04:41:21 +0900

    feat : null 설정 추가
    Former-commit-id: 23d9c86b4ce2c2e69e23d70d9107331f04ec86dd

--- src/main/java/com/e106/reco/global/config/MongoReactiveConfig.java
+++ src/main/java/com/e106/reco/global/config/MongoReactiveConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09a21eae2e50dfffaa53479bc1298ea857cea8d6aa6db506c550ab6b01b999be
-size 1077
+oid sha256:4a907ebe54627a0ff846eae63c163ac8b6a05758ef3e9f82c178fc48bddfb155
+size 988
