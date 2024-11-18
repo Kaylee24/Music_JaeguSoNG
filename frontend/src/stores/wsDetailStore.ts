5fa2126c7a42143973e8f15c9303e235b1808a92
Kaylee <kkbuh2@gmail.com>
2024-11-19 06:01:46 +0900

    [FE] 드래그, offset 완성 / 로직 구현중...
    Former-commit-id: 9de573ed1c13d2371d41a8e6a42dfec30edb11b6

--- frontend/src/stores/wsDetailStore.ts
+++ frontend/src/stores/wsDetailStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46da10552d384668181f2167ded3aefba09c0e668af8ab6a35a2b6bea30dc201
-size 12807
+oid sha256:b96c97d65579e1d2c8ca98e73d14c13670ed3c455d38f2f99d4a7c742f96c049
+size 16965
