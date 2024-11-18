e5ae2fb868e0a0b6435a1ffa161a40a19d3549b4
KHG <skygprud0707@gmail.com>
2024-11-19 05:10:30 +0900

    feat : 유저정보 랜더링 추가
    Former-commit-id: 7f5300f767ac2e09ff47d16c7121762079e6721f

--- src/main/java/com/e106/reco/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/e106/reco/domain/chat/repository/ChatRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38900a2f408fc28966a799a43ac2a75db052581c57d253b2ad68e163f47fc69b
-size 2543
+oid sha256:2c4e747af45c4bad6204d031bbe2d94ada6802037d2759028291eb36dfb03057
+size 2619
--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ba8e8cdd3101aafdfd273f00273b89184c4322b66543b8108b8b5c834f38198
-size 18400
+oid sha256:551cd3ae826c0a543d3d6e0fb68499a94f606a15d91aa7efa7725376a14cda89
+size 18466
