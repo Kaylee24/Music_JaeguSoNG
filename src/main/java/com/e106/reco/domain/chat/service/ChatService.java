ee03933d6522fa4ad5b2cfa5c9564722e444f0a9
KHG <skygprud0707@gmail.com>
2024-11-19 04:59:59 +0900

    feat : null 설정 추가
    Former-commit-id: fb2ff4a5b2b7efba6f48c8d4c699a479f3d58128

--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4579d077062c0a09a03d5f58a6c58e925ab4adbbfe519cc44d0acf986167b533
-size 15462
+oid sha256:de90e5ae45d3d711d24c2df52d43ecadb46347d26e498ab3b204f2ca88211ef8
+size 15320
