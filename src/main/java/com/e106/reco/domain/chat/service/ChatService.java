fcf2e00fdaa596dd410b014785a30fc7ed89ad32
KHG <skygprud0707@gmail.com>
2024-11-18 13:50:26 +0900

    feat : 채팅방 목록 업데이트 수정
    Former-commit-id: ae2edb3e780d833f09844bdbfbc4a0b77f215241

--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:423cfb1192e9ebc70708cd0ad1fa46ecee3aaf8ff8e3b37671ea0c7a0e2b2add
-size 15521
+oid sha256:2015a8dbb0309b664236a2f2abff880919c37051374dd188bab37cf4269b681a
+size 15526
