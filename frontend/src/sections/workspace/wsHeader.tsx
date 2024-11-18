2753e78bb409b8ef90e59abf70d63dcaf1ffd520
김송희 <kkbuh2@gmail.com>
2024-11-18 12:47:53 +0900

    [FE] 워크스페이스 public/private 버튼 개선
    Former-commit-id: e6c56cc372be498d624cb082c2c01e8f5e243791

--- frontend/src/sections/workspace/wsHeader.tsx
+++ frontend/src/sections/workspace/wsHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:deb4b657b2c32c91b6b3b374ce738dbb7cbdd215dddcbc485a08cc82982b051f
-size 9237
+oid sha256:a544baa5ebc98c43f156b781b2a00065e4c2c85c1181a35d000a88f146879d47
+size 9914
