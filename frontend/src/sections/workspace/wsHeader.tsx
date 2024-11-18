0f43d3e81852f0b02a38b0d451ddf83a8c0d9192
김송희 <kkbuh2@gmail.com>
2024-11-18 14:38:38 +0900

    [FE] 워크스페이스 공개 여부 디버깅
    Former-commit-id: 4a30660f76de2c71fc56b5ece0ff5d6478cc3084

--- frontend/src/sections/workspace/wsHeader.tsx
+++ frontend/src/sections/workspace/wsHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a544baa5ebc98c43f156b781b2a00065e4c2c85c1181a35d000a88f146879d47
-size 9914
+oid sha256:9497a41733898c27be6b3fbd86a42654a49aed24cdd7b5e2b6014c2101fa94ba
+size 10204
