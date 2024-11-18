c64145f2860d9a3b5992c9e3b4264bf86fda9f14
Songdoeon <ehdjs9583@naver.com>
2024-11-18 14:10:46 +0900

    [BE] charArtist 주입
    Former-commit-id: 19e3154f25f1ad072597798d4a181038fda812f1

--- src/main/java/com/e106/reco/domain/chat/service/ChatService.java
+++ src/main/java/com/e106/reco/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc4df62c373614d344215f4291d678909bda79774a05a479c958232183710cee
-size 15830
+oid sha256:23dcbeeba2480466bbec4279c8300b28641aa7d6cd0e6c05f66aaa103f2213c3
+size 15843
