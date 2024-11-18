587700bd6a52f16efd6ab164c040d3262a01c1c2
구현우 <kjklovekhw@naver.com>
2024-11-18 10:23:51 +0900

    [FE] feat : 이미지 올리기 수정
    Former-commit-id: f1c87216220947c11e0d8a1c957fc4745a0797dc

--- frontend/src/pages/community/community-detail-view.tsx
+++ frontend/src/pages/community/community-detail-view.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96b0cdf184cd8bb60242e547f2e6c0fa18158effe6d644572bc2470798246533
-size 7712
+oid sha256:600c73c154770b4fc1cd490ed5a8e396a0d7c235f2aec21dae08f6853eb51a0b
+size 7598
