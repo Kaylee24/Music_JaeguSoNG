23437411587976750aa162a222ab57b0d5101db0
김송희 <kkbuh2@gmail.com>
2024-11-18 14:21:30 +0900

    [FE] 워크스페이스 개별 재생 디버깅
    Former-commit-id: 332770160663133ea118d47f426f61b06f90374e

--- frontend/src/components/workspace/session.tsx
+++ frontend/src/components/workspace/session.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67e6da96177fd574dc5162fe8e1ad4829c0a7659699140a72f0b7b7b2539bd6c
-size 19507
+oid sha256:e45b1c4b6fcf8574338fe64caf19caa2ce798f7933cbfbbc2368ff5ad9f4d8c1
+size 20115
