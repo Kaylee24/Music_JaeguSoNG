440d06f981cd7ca0f1313574142f8d340f446172
김송희 <kkbuh2@gmail.com>
2024-11-20 12:27:28 +0900

    [FE] 워크스페이스 기능 디벨롭
    Former-commit-id: 58e81815f859162ec088edacdd9d6945bac36aee

--- frontend/src/stores/wsDetailStore.ts
+++ frontend/src/stores/wsDetailStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe51828d3f7cd843d9dffa8d219af5f2d423951843582750626ff42d615514b6
-size 17324
+oid sha256:ef2d7689b328762ce6ddb014930037276afe648e0c92f9aafb2e5d72a4ae8f61
+size 17282
